4d939c334ab3ae7464c32640d4fcc671920e62fb
dchako <dchako_623@hotmail.com>
2016-04-17 19:21:00 -0300

    add boostrap && angular.js y routing views

--- app/views.py
+++ app/views.py
@@ -5,6 +5,10 @@ from app import app
 def home():
     return "Hellos from Flask"
 
+@app.route("/home")
+def home_2():
+    return render_template('home.html', name="Saludos")
+
 @app.route("/texto/<Saludos>")
 def Saludos(Saludos=None):
-    return render_template('home.html', name=Saludos)
+    return render_template('saludos.html', name=Saludos)
